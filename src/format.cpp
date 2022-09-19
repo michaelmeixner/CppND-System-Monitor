d223b44810387d56efa2e6b65b45c7c136499f93
Michael Meixner <michael.meixner@lmco.com>
2022-09-19 12:04:22 -0400

    changes suggested/necessary from code review: removed extra guidance code

--- src/format.cpp
+++ src/format.cpp
@@ -5,10 +5,6 @@
 using std::string;
 using std::to_string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long int seconds) { 
     int minutes, hours;
     string hourStr, minuteStr, secondStr;
