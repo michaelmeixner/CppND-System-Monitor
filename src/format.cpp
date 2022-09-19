4492a64d78cb27d34416f42d78e0c74c227f9d21
Michael Meixner <michael.meixner@lmco.com>
2022-09-19 12:33:01 -0400

    changes suggested/necessary from code review: updated uptime function to use chrono library

--- src/format.cpp
+++ src/format.cpp
@@ -10,26 +10,17 @@ using std::to_string;
 
 string Format::ElapsedTime(long s) {
   std::chrono::seconds seconds{s};
-
-  // return std::chrono::format("%T", seconds); // in C++20 :-)
-
   std::chrono::hours hours =
       std::chrono::duration_cast<std::chrono::hours>(seconds);
-
   seconds -= std::chrono::duration_cast<std::chrono::seconds>(hours);
-
   std::chrono::minutes minutes =
       std::chrono::duration_cast<std::chrono::minutes>(seconds);
-
   seconds -= std::chrono::duration_cast<std::chrono::seconds>(minutes);
-
   std::stringstream ss{};
-
   ss << std::setw(2) << std::setfill('0') << hours.count()     // HH
      << std::setw(1) << ":"                                    // :
      << std::setw(2) << std::setfill('0') << minutes.count()   // MM
      << std::setw(1) << ":"                                    // :
      << std::setw(2) << std::setfill('0') << seconds.count();  // SS
-
   return ss.str();
 }
