eead83a7c1ba32160e869c17959d3f807c2f3e4e
Michael Meixner <michael.meixner@lmco.com>
2022-09-19 12:19:43 -0400

    changes suggested/necessary from code review: changed return sequence for uptime function

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -283,6 +283,7 @@ string LinuxParser::User(int pid) {
 
 long LinuxParser::UpTime(int pid) {
   string line, value;
+  long uptime;
   vector<string> values;
   std::ifstream stream(kProcDirectory + std::to_string(pid) + kStatFilename);
   if(stream.is_open()) {
